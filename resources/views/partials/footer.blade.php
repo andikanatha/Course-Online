49b5a270895bb684498ebf2fe4870bb9d14e4197
andikanatha <setya.natha@gmail.com>
2023-01-30 20:47:57 +0700

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,63 @@
+<footer class="footer-area">
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-sm-6 col-md-4 col-xl-3">
+                <div class="single-footer-widget footer_1">
+                    <a href="index.html"> <img src="{{ asset('img/course-logo.png') }}" alt=""> </a>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Phasellus egestas tellus rutrum tellus pellentesque eu. Bibendum ut tristique et egestas quis ipsum suspendisse. </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit </p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-xl-4">
+                <div class="single-footer-widget footer_2">
+                    <h4>Suggestion</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </p>
+                    <form action="#">
+                        <div class="form-group">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder='Enter email address'
+                                    onfocus="this.placeholder = ''"
+                                    onblur="this.placeholder = 'Enter email address'">
+                                <div class="input-group-append">
+                                    <button class="btn btn_1" type="button"><i class="ti-angle-right"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    <div class="social_icon">
+                        <a href="#"> <i class="ti-facebook"></i> </a>
+                        <a href="#"> <i class="ti-twitter-alt"></i> </a>
+                        <a href="#"> <i class="ti-instagram"></i> </a>
+                        <a href="#"> <i class="ti-skype"></i> </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 col-md-4">
+                <div class="single-footer-widget footer_2">
+                    <h4>Contact us</h4>
+                    <div class="contact_info">
+                        <p><span> Address :</span> Virtual Office </p>
+                        <p><span> Phone :</span> +62 827-8279-8974</p>
+                        <p><span> Email : </span>ACG@gmail.com </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="copyright_part_text text-center">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <p class="footer-text m-0">
+                                Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Algorithm Computer Group</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
