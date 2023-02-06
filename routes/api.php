2d939a69399ea742ce496c2af78b9eadb9bb78e5
andikanatha <setya.natha@gmail.com>
2023-02-06 21:55:55 +0700

    FIX

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Routing\Route;
 
 Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:api']], function () {
     // Permissions
